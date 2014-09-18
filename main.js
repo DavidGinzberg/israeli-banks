0074594ea0d8c0f1b9fe31244e7b05438d93c06a
David Ginzberg <DavidGinzberg@users.noreply.github.com>
2014-09-17 21:05:59 -0400

    touch main.js
    created main.js with placeholder function.

---
+++ main.js
@@ -0,0 +1,9 @@
+
+/*Build the table of banks based on the JSON data in banks.js*/
+function buildTableOfBanks(){
+    console.log("nothing here yet.");
+}
+
+$(document).ready(function(){
+    buildTableOfBanks();
+});

5add2e9d2da5416a3a7771651e7511239e952607
David Ginzberg <DavidGinzberg@users.noreply.github.com>
2014-09-17 21:48:08 -0400

    Added table loading and construction
    Added ajax call to fetch banks.json and load `name`, `nameInHebrew`, and `openFriday` into the table, which is then added to the body of the page.

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 
 /*Build the table of banks based on the JSON data in banks.js*/
 function buildTableOfBanks(){
-    console.log("nothing here yet.");
+    $.getJSON("banks.json", function(result){
+        var headerRow = $("<tr/>")
+                            .append("<th>Name</th>")
+                            .append("<th>Hebrew Name</th>")
+                            .append("<th>Open Fridays?</th>");
+        $("<table/>").append(headerRow)
+            .append(
+                $.map(result, function(item, index){
+                    return $('<tr/>')
+                        .append('<th>' + item.name + '</th>')
+                        .append('<td>' + item.nameInHebrew + '</td>')
+                        .append('<td>' + item.openFriday?"Yes":item.openFriday==null?"Unknown":"No" + '</td>');
+                    
+                    
+                }))
+            .appendTo('body');
+        
+    });
 }
 
 $(document).ready(function(){

fb8dda974a1487115f6c74bf49beb3db57ab17af
David Ginzberg <DavidGinzberg@users.noreply.github.com>
2014-09-18 00:56:22 -0400

    Refactored table construction
    Refactored the process of table construction to have more one-liners and extract logically different steps into separate functions. Also changed how table was being rendered because the previous method didn't work (was based on a misunderstanding of $.getJSON's return value). hopefully this fixes the latest rendering issue.

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 
-/*Build the table of banks based on the JSON data in banks.js*/
-function buildTableOfBanks(){
-    return $.getJSON("banks.json", function(result){
-        var headerRow = $("<tr/>")
-                            .append("<th>Name</th>")
-                            .append("<th>Hebrew Name</th>")
-                            .append("<th>Open Fridays?</th>");
-        $("<table/>").append(headerRow)
-            .append(
-                $.map(result, function(item, index){
-                    return $('<tr/>')
-                        .append('<th>' + item.name + '</th>')
-                        .append('<td>' + item.nameInHebrew + '</td>')
-                        .append('<td>' + (item.openFriday==null?"Unknown":item.openFriday?"Yes":"No") + '</td>');
-                    
-                    
-                }));
-        
-    });
+function renderTable(table){
+    $("#tableContainer").html(table);
 }
 
+function constructTable(data){
+    var headerRow = $("<tr/>")
+            .append("<th>Name</th>")
+            .append("<th>Hebrew Name</th>")
+            .append("<th>Open Fridays?</th>");
+    return $("<table/>").append(headerRow)
+        .append(
+            $.map(data, function(item, index){
+                return $('<tr/>')
+                    .append('<th>' + item.name + '</th>')
+                    .append('<td>' + item.nameInHebrew + '</td>')
+                    .append('<td>' + (item.openFriday==null?"Unknown":item.openFriday?"Yes":"No") + '</td>');
+
+            }));
+}
+
+/*Build the table of banks based on the JSON data in banks.js*/
+function buildTableOfBanks(){
+    $.getJSON("banks.json", function(result){ renderTable(constructTable(result))});
+}
+
+
 $(document).ready(function(){
-    $("#tableContainer").html(buildTableOfBanks());
+    buildTableOfBanks();
 });
